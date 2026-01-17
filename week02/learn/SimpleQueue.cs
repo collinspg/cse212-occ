2695d214156f5de711069df838d9226d007eb95c
collinspg <collinsokolie90@gmail.com>
2026-01-17 02:32:10 +0100

    Completed W02 Learning Activity: Finding Defects using Tests

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value); // FIXED: Add to the back of the queue, not the front
     }
 
     /// <summary>
@@ -66,8 +66,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0]; // FIXED: Get the front element (index 0)
+        _queue.RemoveAt(0);     // FIXED: Remove the front element (index 0)
         return value;
     }
 }
